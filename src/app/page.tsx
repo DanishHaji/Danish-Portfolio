e1c0a7cf0e6bafa73642b043f7c7ed27d360bf45
DanishHaji <danish.haji111@gmail.com>
2025-05-05 02:00:35 +0500

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar"
 import Hero from "@/components/Hero"
+import CardGrid from "@/components/CardGrid";
 
 export default function Home() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <CardGrid />
         </div>
     );
 }
